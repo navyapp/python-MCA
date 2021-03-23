94ec9a83b12ee8f2b50ab88f4739d021099f2af3
navyapp <79969396+navyapp@users.noreply.github.com>
2021-03-23 20:28:50 +0530

    Delete co1 leapyear(2).py

--- C01/co1 leapyear(2).py
+++
@@ -1,7 +0,0 @@
-# co1, 2
-y1 = int(input("ener the current year"))
-y2 = int(input("enter the last year"))
-for i in range(y1, y2 + 1):
-    if (i % 4 == 0 and i % 100 != 0 or i % 400 == 0):
-        print(i)
-    i = i + 1
