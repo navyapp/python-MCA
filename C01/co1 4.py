542bd7d0852a3374d43cd6bc3922949b9820b116
navyapp <79969396+navyapp@users.noreply.github.com>
2021-03-23 20:26:50 +0530

    Delete co1 4.py

--- C01/co1 4.py
+++
@@ -1,8 +0,0 @@
-#cos 6
-string=input("enter any string:")
-c=input("enter te character to the string")
-count =0
-for i in string:
-  if i == c:
-    count = count +1
-print(c,"occurs",count,"times")
